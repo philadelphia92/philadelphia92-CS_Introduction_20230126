ada1d419d62df741989559ffc5322983e42b3a9d
Igor <exetto92@gmail.com>
2023-01-29 22:11:02 +0300

    Lesson1 02

---
+++ 011/Program.cs
@@ -0,0 +1,19 @@
+﻿// 11. Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+
+int n=123;
+int d0,d1,d2;
+d0=n%10;
+d1=n/10%10;
+d2=n/100;
+System.Console.WriteLine($"{d0} {d1} {d2}");
+
+//7. Выяснить является ли число четным.
+int N2=10;
+bool odd= N2%2==0;
+if (odd==true)
+{
+System.Console.WriteLine($"Число {N2} четное");
+}
+else{
+System.Console.WriteLine($"Число {N2} не четное");
+}
