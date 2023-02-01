ee132574d31342a50467662b20cd5add5017af6f
Igor <exetto92@gmail.com>
2023-02-01 19:14:45 +0300

    Lesson1 a Task 8

---
+++ 008/Program.cs
@@ -0,0 +1,12 @@
+﻿// 8. Вывести на экран числа от -N до N
+int N=3;
+int i=-N;
+Console.WriteLine("Введите число");
+N=Convert.ToInt32(Console.ReadLine());
+
+while(i<=N)
+{
+Console.WriteLine(i);
+i=i+1;
+}
+
