b090fc36b2fe16d449434bbd5fc76c19de1257d8
Igor <exetto92@gmail.com>
2023-02-01 19:31:01 +0300

    Lesson1 a Task 13

---
+++ 013/Program.cs
@@ -0,0 +1,13 @@
+﻿// 13. Удалить вторую цифру целого числа введенного с клавиатуры
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите позицию");
+int s = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+while (s > 1) {
+m *= 10;
+s--;
+}
+int t = (x % m) + (x / (m * 10)) * m;
+Console.WriteLine(t);
