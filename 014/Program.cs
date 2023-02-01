5e839f71ada8fc00ea3b1c78e1102fb2ff4040e0
Igor <exetto92@gmail.com>
2023-02-01 19:59:51 +0300

    Lesson1 a Task 14

---
+++ 014/Program.cs
@@ -0,0 +1,14 @@
+﻿// 14. С клавиатуры вводятся два числа a и b. Выяснить, кратно ли число a числу b, если нет, вывести остаток от деления a на b.
+
+int remainder;
+Console.WriteLine("Введите число a");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b=int.Parse(Console.ReadLine());
+remainder=a%b;
+if(remainder !=0)
+{
+    Console.WriteLine($"{remainder} - делится с остатком. Остаток:");
+
+}
+Console.WriteLine((double)a/(double)b);
