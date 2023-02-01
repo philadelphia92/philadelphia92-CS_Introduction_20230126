2c51a92d6540a4ae4c1e153378f101c85c595be6
Igor <exetto92@gmail.com>
2023-02-01 20:30:55 +0300

    Lesson1 a Task 15

---
+++ 015/Program.cs
@@ -0,0 +1,8 @@
+﻿// 15. С клавиатуры вводится целое число. Вывести третью цифру числа или сообщить, что её нет
+
+System.Console.Write("Введите число:");
+string a = System.Console.ReadLine();
+if (a.Length>2)
+System.Console.WriteLine(a[2]);
+else
+System.Console.WriteLine("NO");
