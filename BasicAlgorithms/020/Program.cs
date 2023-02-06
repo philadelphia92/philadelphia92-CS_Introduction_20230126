5a14debd88a1ce57a3d5da3f1b3fc660fce87ae9
Igor <exetto92@gmail.com>
2023-02-06 13:19:27 +0300

    Lesson2 a Task 20

---
+++ BasicAlgorithms/020/Program.cs
@@ -0,0 +1,14 @@
+﻿// 20. Дано число. Проверить кратно ли оно 7 и 23
+
+int number = 777;
+int a = 7;
+int b = 23;
+string txtResult = " ";
+
+if (number % a == 0 && number % b == 0) txtResult = "кратно обоим числам";
+else if (number % a != 0 && number % b != 0) txtResult = "не кратно ни одному из чисел";
+else if (number % a == 0) txtResult = $"кратно только числу {a}";
+else txtResult = $"кратно только числу {b}";
+
+System.Console.WriteLine($"Проверить кратность числа {number} числам {a} и {b} ");
+System.Console.WriteLine($"Число {number} {txtResult} ");
---
+++ BasicAlgorithms/BOOL/Program.cs
@@ -0,0 +1,5 @@
+﻿System.Int32 i32;
+System.Int16 i16;
+System.Int64 i64;
+System.Int128 i128;
+Console.WriteLine($"{System.Int16.Min} {System.Int16.Max});
