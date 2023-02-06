25008922f8496fdc3522a47fbc96b3c039339def
Igor <exetto92@gmail.com>
2023-02-06 13:57:37 +0300

    Lesson2 a Task 26

---
+++ BasicAlgorithms/026/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа проверяет пятизначное число на палиндромом. Например: 123321
+
+System.Console.Write("Введите пятизначное число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+string txtToCheck = number.Replace(" ","");
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+System.Console.WriteLine("Ответ:");
+if (IsPalindrome()) System.Console.WriteLine($"Число - {number} - является палиндромом");
+else System.Console.WriteLine($"Число {number} не является палиндромом");
